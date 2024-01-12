faaacdfc1998ce48c6bc92719fd46cbc654974dc
daniyal9407 <muhammad.daniyal@metafroliclabs.com>
2024-01-12 18:17:47 +0500

    initial commit

---
+++ js/index.js
@@ -0,0 +1,31 @@
+$('.testi-carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+});
+
+
+var owl = $('.owl-carousel');
+owl.owlCarousel();
+// Go to the next item
+$('.customNextBtn').click(function() {
+    owl.trigger('next.owl.carousel');
+})
+// Go to the previous item
+$('.customPrevBtn').click(function() {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owl.trigger('prev.owl.carousel', [300]);
+})
